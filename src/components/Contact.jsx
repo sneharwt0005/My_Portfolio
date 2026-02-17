8133152a08c2c59d2f449eccc2c16efef7c8573b
Sneha Rawat <rwts38549@gmail.com>
2026-02-17 14:35:25 +0530

    edit frontend api

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,7 @@ function Contact() {
     setSuccess("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/contact`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
