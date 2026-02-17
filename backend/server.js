a71b4cf7159f5daf10dd086db1e44305437a9165
Sneha Rawat <rwts38549@gmail.com>
2026-02-17 15:22:11 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "https://my-portfolio-eta-nine-58.vercel.app",
+  origin: "https://my-portfolio-drab-xi-22.vercel.app/",
   methods: ["GET", "POST"],
   credentials: true
 }));
